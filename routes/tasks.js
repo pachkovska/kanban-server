fa2b7c8ec6a93ba7b1da2f06c9919081e0d1ae8f
pachkovska <pachkovska@gmail.com>
2020-05-07 22:05:45 -0700

    add patch route to update existing task

--- routes/tasks.js
+++ routes/tasks.js
@@ -33,30 +33,35 @@ router.post('/', (req, res) => {
     });
 });
 
-router.get('/:taskId', (req, res) => {
-    const id = req.params.taskId;
-    if (id === 'special') {
-        res.status(200).json({
-            message: 'Handling GET requests to /tasks for a special ID'
-        });
-    } else {
-        res.status(200).json({
-            message: "Id was passed, but not found in Database"
-        })
-    }
-});
+// router.get('/:taskId', (req, res) => {
+//     const id = req.params.taskId;
+//     if (id === 'special') {
+//         res.status(200).json({
+//             message: 'Handling GET requests to /tasks for a special ID'
+//         });
+//     } else {
+//         res.status(200).json({
+//             message: "Id was passed, but not found in Database"
+//         })
+//     }
+// });
 
 router.patch('/:taskId', (req, res, next) => {
     const id = req.params.taskId;
-    if (id === 'special') {
-        res.status(200).json({
-            message: 'Updated task in database'
-        });
-    } else {
-        res.status(200).json({
-            message: "Patch request was sent for taskId that does not exist"
+    console.log(req.body)
+    Task.updateOne({_id: id}, {
+        $set: {
+            taskTitle: req.body.taskTitle,
+            taskBody: req.body.taskBody
+        }
+    })
+        .exec()
+        .then(docs => {
+            res.status(200).json({tasks: docs});
         })
-    }
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
 router.post('/:taskId', (req, res, next) => {
